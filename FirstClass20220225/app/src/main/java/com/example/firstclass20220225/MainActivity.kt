17cb821ce4c0341dae65a107439a7054c981a37e
郵差 <twHenryYang@gmail.com>
2022-02-25 11:41:52 +0800

    Create FirstClass practice

---
+++ FirstClass20220225/app/src/main/java/com/example/firstclass20220225/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firstclass20220225
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
