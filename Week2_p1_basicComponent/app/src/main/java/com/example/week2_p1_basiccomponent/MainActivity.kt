a614feaa9eae5c0e49db53406affaa72cb23ee0b
郵差 <twHenryYang@gmail.com>
2022-03-04 10:32:56 +0800

    Finish pratice1-1 add basic components to layout

--- Week2_p1_basicComponent/app/src/main/java/com/example/week2_p1_basiccomponent/MainActivity.kt
+++ Week2_p1_basicComponent/app/src/main/java/com/example/week2_p1_basiccomponent/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.week2_p1_basiccomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView1:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 從資源類別R中取得介面元件
+        textView1 = findViewById<TextView>(R.id.tv1);
+        textView1.text = "Hello Startup success!";
     }
 }
