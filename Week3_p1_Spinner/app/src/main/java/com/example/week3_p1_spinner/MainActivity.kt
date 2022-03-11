fcd49c887816bf16849a3fcfdcaa9c26b526cad2
郵差 <twHenryYang@gmail.com>
2022-03-11 09:42:17 +0800

    Create Week3 practice 1 Spinner

---
+++ Week3_p1_Spinner/app/src/main/java/com/example/week3_p1_spinner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.week3_p1_spinner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
