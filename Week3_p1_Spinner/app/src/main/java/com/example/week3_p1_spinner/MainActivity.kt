f631d00f12a9f897d941ec9f7d9dedccb074163a
郵差 <twHenryYang@gmail.com>
2022-03-11 10:22:57 +0800

    load layout components in MainActivity

--- Week3_p1_Spinner/app/src/main/java/com/example/week3_p1_spinner/MainActivity.kt
+++ Week3_p1_Spinner/app/src/main/java/com/example/week3_p1_spinner/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.week3_p1_spinner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var spinner1: Spinner
+    private lateinit var spinner2: Spinner
+    private lateinit var button1: Button
+    private lateinit var output: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get layout components from class R (resource)
+        spinner1 = findViewById(R.id.spinner1)
+        spinner2 = findViewById(R.id.spinner2)
+        button1 = findViewById(R.id.btn1)
+        output = findViewById(R.id.tv1)
     }
 }
