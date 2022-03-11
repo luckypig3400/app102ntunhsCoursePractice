e6cc6642b987e331ec235d8010c95a187ccda092
郵差 <twHenryYang@gmail.com>
2022-03-11 11:10:56 +0800

    feat: selected event listener for Spinner1

--- Week3_p1_Spinner/app/src/main/java/com/example/week3_p1_spinner/MainActivity.kt
+++ Week3_p1_Spinner/app/src/main/java/com/example/week3_p1_spinner/MainActivity.kt
@@ -2,16 +2,15 @@ package com.example.week3_p1_spinner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var spinner1: Spinner
     private lateinit var spinner2: Spinner
     private lateinit var button1: Button
     private lateinit var output: TextView
+    private lateinit var debugInfo: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         spinner2 = findViewById(R.id.spinner2)
         button1 = findViewById(R.id.btn1)
         output = findViewById(R.id.tv1)
+        debugInfo = findViewById(R.id.debuginfo)
 
         // Connect the content with ArrayAdapter
         var adapterSpinner1 = ArrayAdapter.createFromResource(
@@ -36,5 +36,21 @@ class MainActivity : AppCompatActivity() {
         // Bind the adapter to spinner1
         spinner1.adapter = adapterSpinner1
 
+        // EventListener for spinner1 (unnecessary)
+        spinner1.setOnItemSelectedListener(SpinnerSelectedListener_spinner1)
     }
+
+    // EventListener for spinner1 (unnecessary)
+    private val SpinnerSelectedListener_spinner1: AdapterView.OnItemSelectedListener =
+        object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View, pos: Int, id: Long) {
+                // 選到時做的事 (pos會傳回第N個整數)
+                debugInfo.text = "Spinner1 eListener:"+parent?.selectedItem.toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // 都沒被選到時
+                debugInfo.text = "Nothing selected"
+            }
+        }
 }
