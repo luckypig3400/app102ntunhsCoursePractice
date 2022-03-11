401557e2a2b7e82b79850c166bb9b6f36c81fe28
郵差 <twHenryYang@gmail.com>
2022-03-11 10:35:52 +0800

    update: define items for spinner1 & bind with Adaptor

--- Week3_p1_Spinner/app/src/main/java/com/example/week3_p1_spinner/MainActivity.kt
+++ Week3_p1_Spinner/app/src/main/java/com/example/week3_p1_spinner/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.week3_p1_spinner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
@@ -16,10 +17,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get layout components from class R (resource)
+        // Get layout components from class R (resource)
         spinner1 = findViewById(R.id.spinner1)
         spinner2 = findViewById(R.id.spinner2)
         button1 = findViewById(R.id.btn1)
         output = findViewById(R.id.tv1)
+
+        // Connect the content with ArrayAdapter
+        var adapter_spinner1 = ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner1_content,
+            android.R.layout.simple_spinner_item
+        )
+
+        // Set style for spinner1 (unnecessary)
+        adapter_spinner1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Bind the adapter to spinner1
+        spinner1.adapter = adapter_spinner1
+
     }
 }
