07ac421fc07b4b0afe5c71a5ea394ec145232093
郵差 <twHenryYang@gmail.com>
2022-03-12 14:08:15 +0800

    feat: initial Random number

--- Week2_hw1_BingoGame/app/src/main/java/com/example/week2_hw1_bingogame/MainActivity.kt
+++ Week2_hw1_BingoGame/app/src/main/java/com/example/week2_hw1_bingogame/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.week2_hw1_bingogame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var input: EditText
     private lateinit var imgBtn: ImageButton
     private lateinit var output: TextView
+
+    private var bingoNumber = Random.nextInt(1, 101)
+    //  https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.random/-random/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +25,11 @@ class MainActivity : AppCompatActivity() {
         imgBtn = findViewById(R.id.imageButton)
         output = findViewById(R.id.output)
 
+        output.text = bingoNumber.toString()
     }
 
+//    private var buttonBingoGame = View.OnClickListener {
+//
+//    }
 
 }
