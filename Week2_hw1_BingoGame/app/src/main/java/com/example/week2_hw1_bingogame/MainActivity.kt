09c3f4487328dc268a1b006eefc4f08bd4202f00
郵差 <twHenryYang@gmail.com>
2022-03-12 13:59:35 +0800

    chore: initial layout components

--- Week2_hw1_BingoGame/app/src/main/java/com/example/week2_hw1_bingogame/MainActivity.kt
+++ Week2_hw1_BingoGame/app/src/main/java/com/example/week2_hw1_bingogame/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.week2_hw1_bingogame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var input: EditText
+    private lateinit var imgBtn: ImageButton
+    private lateinit var output: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initial layout components
+        input = findViewById(R.id.input)
+        imgBtn = findViewById(R.id.imageButton)
+        output = findViewById(R.id.output)
+
     }
+
+
 }
