d45262b2ee3e3b21e4963e9fad2cf2b7313dea12
郵差 <twHenryYang@gmail.com>
2022-03-13 09:11:01 +0800

    feat: provide restart game function

--- Week2_hw1_BingoGame/app/src/main/java/com/example/week2_hw1_bingogame/MainActivity.kt
+++ Week2_hw1_BingoGame/app/src/main/java/com/example/week2_hw1_bingogame/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.week2_hw1_bingogame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var input: EditText
-    private lateinit var imgBtn: ImageButton
+    private lateinit var bingoBtn: Button
     private lateinit var output: TextView
 
     private var bingoNumber = Random.nextInt(1, 101)
@@ -22,21 +22,32 @@ class MainActivity : AppCompatActivity() {
 
         // initial layout components
         input = findViewById(R.id.input)
-        imgBtn = findViewById(R.id.imageButton)
+        bingoBtn = findViewById(R.id.bingoBtn)
         output = findViewById(R.id.output)
 
         // Bind onClick Event Listener to imageButton
-        imgBtn.setOnClickListener(buttonBingoGame)
+        bingoBtn.setOnClickListener(buttonBingoGame)
     }
 
     private var buttonBingoGame = View.OnClickListener {
         var num = input.text.toString().toInt()
 
-        if(num > bingoNumber){
+        if (num > bingoNumber) {
             output.text = "數值太大"
-        }else if(num == bingoNumber){
+        } else if (num == bingoNumber) {
             output.text = "Bingo~!"
-        }else{
+            input.isEnabled = false
+
+            if (bingoBtn.text.toString() == "再來一局") {
+                bingoNumber = Random.nextInt(1, 101)
+
+                output.text = "來吧~繼續遊玩猜數字( •̀ ω •́ )✧"
+                bingoBtn.text = "我猜是這個數字"
+                input.isEnabled = true
+            }else{
+                bingoBtn.text = "再來一局"
+            }
+        } else {
             output.text = "數值太小"
         }
 
