019e17c42bd68bb60efff17b24b80d1477ba6804
郵差 <twHenryYang@gmail.com>
2022-03-25 19:07:36 +0800

    Create Week4 practice2 CheckBox

---
+++ Week4_p2_/app/src/main/java/com/example/week4_p2_CheckBox/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.week4_p2_CheckBox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
