76a4f49fb77b28fdae8d3fc906da2f8b0ff0ff51
郵差 <twHenryYang@gmail.com>
2022-03-25 09:37:32 +0800

    Create Week5 practice 1 SharedPreferences

---
+++ Week5_p1_SharedPreferences/app/src/main/java/com/example/week5_p1_sharedpreferences/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.week5_p1_sharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
