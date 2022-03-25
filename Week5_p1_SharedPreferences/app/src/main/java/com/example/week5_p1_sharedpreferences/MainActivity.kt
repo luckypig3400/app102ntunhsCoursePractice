a1e3a3aa7fd049488277ac8d96880c7fe51e40f8
郵差 <a34007024@gmail.com>
2022-03-25 10:49:57 +0800

    chore: initial layout components

--- Week5_p1_SharedPreferences/app/src/main/java/com/example/week5_p1_sharedpreferences/MainActivity.kt
+++ Week5_p1_SharedPreferences/app/src/main/java/com/example/week5_p1_sharedpreferences/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.week5_p1_sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var input: EditText
+    private lateinit var storeBtn: Button
+    private lateinit var showBtn: Button
+    private lateinit var output: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        input = findViewById(R.id.input)
+        storeBtn = findViewById(R.id.storeDataBtn)
+        showBtn = findViewById(R.id.showDataBtn)
+        output = findViewById(R.id.output)
+
     }
 }
