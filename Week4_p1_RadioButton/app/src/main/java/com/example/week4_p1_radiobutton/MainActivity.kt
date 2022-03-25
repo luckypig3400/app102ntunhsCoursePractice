0ff6a94c914be75dd0d0a53253f2edbaacfae85e
郵差 <twHenryYang@gmail.com>
2022-03-25 19:03:04 +0800

    feat: button onClick listener (Finished Week4 p1)

--- Week4_p1_RadioButton/app/src/main/java/com/example/week4_p1_radiobutton/MainActivity.kt
+++ Week4_p1_RadioButton/app/src/main/java/com/example/week4_p1_radiobutton/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.week4_p1_radiobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -22,10 +23,17 @@ class MainActivity : AppCompatActivity() {
 
         // 設定Radio元件的Listener事件
         radioGroup1.setOnCheckedChangeListener(myRadioChecked)
+
+        button.setOnClickListener(buttonClicked)
     }
 
     private val myRadioChecked = RadioGroup.OnCheckedChangeListener { group, checkedID ->
-        val radioB:RadioButton = findViewById(checkedID)
+        val radioB: RadioButton = findViewById(checkedID)
         output.text = "剛按下" + radioB.text
+    }
+
+    private val buttonClicked = View.OnClickListener {
+        val rGroupChecked: RadioButton = findViewById(radioGroup1.checkedRadioButtonId)
+        output.text = "您選擇並送出的為:" + rGroupChecked.text
     }
 }
