4102f1907bc2efec1ba29a09cad3f154b660a19d
郵差 <twHenryYang@gmail.com>
2022-03-25 19:44:41 +0800

    chore: initial layout components

--- Week4_p2_/app/src/main/java/com/example/week4_p2_CheckBox/MainActivity.kt
+++ Week4_p2_/app/src/main/java/com/example/week4_p2_CheckBox/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.week4_p2_CheckBox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var output: TextView
+    private lateinit var button: Button
+    private lateinit var checkBox1: CheckBox
+    private lateinit var checkBox2: CheckBox
+    private lateinit var checkBox3: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        output = findViewById(R.id.output)
+        button = findViewById(R.id.button)
+        checkBox1 = findViewById(R.id.checkBox1)
+        checkBox2 = findViewById(R.id.checkBox2)
+        checkBox3 = findViewById(R.id.checkBox3)
+
     }
 }
