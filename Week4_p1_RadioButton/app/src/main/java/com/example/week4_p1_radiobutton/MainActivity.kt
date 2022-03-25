5f10cdac766e20501afffeb03ade4ca23ea7d3a1
郵差 <twHenryYang@gmail.com>
2022-03-25 18:57:20 +0800

    feat: onCheckedChangeListener for RadioGroup1

--- Week4_p1_RadioButton/app/src/main/java/com/example/week4_p1_radiobutton/MainActivity.kt
+++ Week4_p1_RadioButton/app/src/main/java/com/example/week4_p1_radiobutton/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.week4_p1_radiobutton
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 
@@ -19,5 +20,12 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.button)
         output = findViewById(R.id.output)
 
+        // 設定Radio元件的Listener事件
+        radioGroup1.setOnCheckedChangeListener(myRadioChecked)
+    }
+
+    private val myRadioChecked = RadioGroup.OnCheckedChangeListener { group, checkedID ->
+        val radioB:RadioButton = findViewById(checkedID)
+        output.text = "剛按下" + radioB.text
     }
 }
