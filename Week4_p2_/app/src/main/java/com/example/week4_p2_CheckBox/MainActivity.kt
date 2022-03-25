5f153e47b6a6f5f528cdc3ccf17c2ee8b765946b
郵差 <twHenryYang@gmail.com>
2022-03-25 19:49:56 +0800

    feat: onCheckedChangeListener for CheckBox

--- Week4_p2_/app/src/main/java/com/example/week4_p2_CheckBox/MainActivity.kt
+++ Week4_p2_/app/src/main/java/com/example/week4_p2_CheckBox/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.CompoundButton
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -23,5 +24,13 @@ class MainActivity : AppCompatActivity() {
         checkBox2 = findViewById(R.id.checkBox2)
         checkBox3 = findViewById(R.id.checkBox3)
 
+        // 設定CheckBox元件的Listener事件
+        checkBox1.setOnCheckedChangeListener(myCheckBox1)
+    }
+
+    private val myCheckBox1 = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+        if(isChecked){
+            output.text = checkBox1.text.toString() + "已選取"
+        }
     }
 }
