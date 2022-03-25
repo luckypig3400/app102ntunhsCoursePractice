19f91b160e5a823c4cbe5a4c071a93edbb2cc937
郵差 <twHenryYang@gmail.com>
2022-03-25 18:34:01 +0800

    chore: initial layout components

--- Week4_p1_RadioButton/app/src/main/java/com/example/week4_p1_radiobutton/MainActivity.kt
+++ Week4_p1_RadioButton/app/src/main/java/com/example/week4_p1_radiobutton/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.week4_p1_radiobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var radioGroup1: RadioGroup
+    private lateinit var button: Button
+    private lateinit var output: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        radioGroup1 = findViewById(R.id.radioGroup1)
+        button = findViewById(R.id.button)
+        output = findViewById(R.id.output)
+
     }
 }
