159901b0aebfeaa41ded4660ae93cbaf0e64709b
郵差 <twHenryYang@gmail.com>
2022-04-01 10:27:39 +0800

    chore: initial layout components

--- Week6_p1_saveFile/app/src/main/java/com/example/week6_p1_savefile/MainActivity.kt
+++ Week6_p1_saveFile/app/src/main/java/com/example/week6_p1_savefile/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.week6_p1_savefile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var saveFileBtn: Button
+    lateinit var loadFileBtn: Button
+    lateinit var input: EditText
+    lateinit var output: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        saveFileBtn = findViewById(R.id.saveFileButton)
+        loadFileBtn = findViewById(R.id.loadFileButton)
+        input = findViewById(R.id.input)
+        output = findViewById(R.id.output)
     }
 }
