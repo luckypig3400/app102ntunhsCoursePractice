66fad99d3871a442a6bf1ce07895e68e512bf04d
郵差 <twHenryYang@gmail.com>
2022-04-08 12:59:06 +0800

    feat: create DB & table while initial App

--- Week7_p1_WriteRead_SQLite/app/src/main/java/com/example/week7_p1_writeread_sqlite/MainActivity.kt
+++ Week7_p1_WriteRead_SQLite/app/src/main/java/com/example/week7_p1_writeread_sqlite/MainActivity.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.database.sqlite.SQLiteDatabase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var input: EditText
     private lateinit var writeBtn: Button
     private lateinit var readBtn: Button
     private lateinit var output: TextView
+
+    private val DB_file = "sqliteTest.db"
+    private val DB_table = "testTable"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,21 @@ class MainActivity : AppCompatActivity() {
 
         writeBtn.setOnClickListener(writeDBbuttonClicked)
         readBtn.setOnClickListener(readDBbuttonClicked)
+
+        val MyDB: SQLiteDatabase
+
+        // customize DB Helper Object
+        val MyDBhp = MyDBHelper(applicationContext, DB_file, null, 1)
+
+        // set SQL command for Create Table
+        MyDBhp.sCreateTableCommand = "CREATE TABLE" + DB_table + "(" +
+                "id INTEGER PRIMARY KEY," +
+                "name TEXT NOT NULL," +
+                "sex TEXT," +
+                "address TEXT)"
+
+        // 取得上面指定的檔名資料庫，如果該檔名不存在就會自動建立一個資料庫檔案
+        MyDB = MyDBhp.writableDatabase
     }
 
     private val writeDBbuttonClicked = View.OnClickListener {
@@ -31,6 +50,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val readDBbuttonClicked = View.OnClickListener {
-        
+
     }
 }
