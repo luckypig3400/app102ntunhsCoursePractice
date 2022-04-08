e4847c1588e239c7eb7ef47aaf46d62068a803cf
郵差 <twHenryYang@gmail.com>
2022-04-08 10:19:23 +0800

    Create Week7 practice 1 SQLite Write/Read

---
+++ Week7_p1_WriteRead_SQLite/app/src/main/java/com/example/week7_p1_writeread_sqlite/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.week7_p1_writeread_sqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
