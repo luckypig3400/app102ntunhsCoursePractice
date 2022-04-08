55f31b02d3a685fed1ba5d0cc80228518a166cc7
郵差 <twHenryYang@gmail.com>
2022-04-08 11:59:13 +0800

    chore: initial Layout Components & Btn OnClick Listener

--- Week7_p1_WriteRead_SQLite/app/src/main/java/com/example/week7_p1_writeread_sqlite/MainActivity.kt
+++ Week7_p1_WriteRead_SQLite/app/src/main/java/com/example/week7_p1_writeread_sqlite/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.week7_p1_writeread_sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var input: EditText
+    private lateinit var writeBtn: Button
+    private lateinit var readBtn: Button
+    private lateinit var output: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        input = findViewById(R.id.input)
+        writeBtn = findViewById(R.id.writeDBbutton)
+        readBtn = findViewById(R.id.readDBbutton)
+        output = findViewById(R.id.output)
+
+        writeBtn.setOnClickListener(writeDBbuttonClicked)
+        readBtn.setOnClickListener(readDBbuttonClicked)
+    }
+
+    private val writeDBbuttonClicked = View.OnClickListener {
+
+    }
+
+    private val readDBbuttonClicked = View.OnClickListener {
+        
     }
 }
