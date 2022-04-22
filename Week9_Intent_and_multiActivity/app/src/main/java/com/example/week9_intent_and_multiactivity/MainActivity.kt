0202c357e4977ab7b5ce0db1fda1ba7136d112be
郵差 <twHenryYang@gmail.com>
2022-04-22 10:29:44 +0800

    feat: Example 1 one Activity, Multi Layouts

--- Week9_Intent_and_multiActivity/app/src/main/java/com/example/week9_intent_and_multiactivity/MainActivity.kt
+++ Week9_Intent_and_multiActivity/app/src/main/java/com/example/week9_intent_and_multiactivity/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.week9_intent_and_multiactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        jumpToLayout02()
+    }
+
+    fun jumpToLayout02() {
+        setContentView(R.layout.activity_slave)
+        // 設定button元件的Listener事件
+        this.findViewById<Button>(R.id.button2).setOnClickListener(View.OnClickListener {
+            this@MainActivity.jumpToLayout01()
+        })
+    }
+
+    fun jumpToLayout01() {
+        setContentView(R.layout.activity_main)
+        // 設定button元件的Listener事件
+        this.findViewById<Button>(R.id.button1).setOnClickListener(View.OnClickListener {
+            this@MainActivity.jumpToLayout02()
+        })
     }
 }
+
