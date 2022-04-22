5494c6987b21eb9b1f4ad835c10144670804f045
郵差 <twHenryYang@gmail.com>
2022-04-22 10:49:51 +0800

    feat: Example2 use LayoutInflater to change Layouts in 1 Activity

--- Week9_Intent_and_multiActivity/app/src/main/java/com/example/week9_intent_and_multiactivity/MainActivity.kt
+++ Week9_Intent_and_multiActivity/app/src/main/java/com/example/week9_intent_and_multiactivity/MainActivity.kt
@@ -8,11 +8,25 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        jumpToLayout02()
+        val inflater = layoutInflater
+
+        val view1: View = inflater.inflate(R.layout.activity_main, null) //找出第一個視窗
+        val view2: View = inflater.inflate(R.layout.activity_slave, null) //找出第二個視窗
+        setContentView(view1) //顯示第一個視窗
+
+        val button1 = view1.findViewById<View>(R.id.button1) as Button //第一個視窗的按鈕
+        button1.setOnClickListener(View.OnClickListener () {
+            setContentView(view2) //按了之後跳到第2個視窗
+        })
+
+        val button2 = view2.findViewById<View>(R.id.button2) as Button //第二個視窗的按鈕
+        button2.setOnClickListener(View.OnClickListener() {
+            setContentView(view1) //按了之後跳到第1個視窗
+        })
     }
 
+    /*
     fun jumpToLayout02() {
         setContentView(R.layout.activity_slave)
         // 設定button元件的Listener事件
@@ -28,5 +42,5 @@ class MainActivity : AppCompatActivity() {
             this@MainActivity.jumpToLayout02()
         })
     }
+    */
 }
-
