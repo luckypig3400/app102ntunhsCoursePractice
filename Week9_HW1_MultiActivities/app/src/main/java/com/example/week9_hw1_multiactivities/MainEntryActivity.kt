5d12f078813e89a231a37bb3b136f55865008366
郵差 <twHenryYang@gmail.com>
2022-05-18 22:26:06 +0800

    feat: change between Activities

--- Week9_HW1_MultiActivities/app/src/main/java/com/example/week9_hw1_multiactivities/MainEntryActivity.kt
+++ Week9_HW1_MultiActivities/app/src/main/java/com/example/week9_hw1_multiactivities/MainEntryActivity.kt
@@ -1,7 +1,9 @@
 package com.example.week9_hw1_multiactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainEntryActivity : AppCompatActivity() {
@@ -16,5 +18,28 @@ class MainEntryActivity : AppCompatActivity() {
         navbarBtn = findViewById(R.id.navbarActButton)
         mapBtn = findViewById(R.id.mapActButton)
         coolBtn = findViewById(R.id.rwdActButton)
+
+        navbarBtn.setOnClickListener(navbarBtnListener)
+        mapBtn.setOnClickListener(mapBtnListener)
+        coolBtn.setOnClickListener(coolBtnListener)
     }
+
+    val navbarBtnListener = View.OnClickListener {
+        val intent = Intent()
+        intent.setClass(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    var mapBtnListener = View.OnClickListener {
+        val intent = Intent()
+        intent.setClass(this, MapsActivity::class.java)
+        startActivity(intent)
+    }
+
+    var coolBtnListener = View.OnClickListener {
+        val intent = Intent()
+        intent.setClass(this, SomethingCoolActivity::class.java)
+        startActivity(intent)
+    }
+
 }
