f78f9896cbb45f28827d76e03d5b78635f1cdfa6
郵差 <twHenryYang@gmail.com>
2022-05-18 22:20:10 +0800

    chore: initial layout components

--- Week9_HW1_MultiActivities/app/src/main/java/com/example/week9_hw1_multiactivities/MainEntryActivity.kt
+++ Week9_HW1_MultiActivities/app/src/main/java/com/example/week9_hw1_multiactivities/MainEntryActivity.kt
@@ -5,14 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainEntryActivity : AppCompatActivity() {
-    private lateinit var coolBtn: Button
+    private lateinit var navbarBtn: Button
     private lateinit var mapBtn: Button
+    private lateinit var coolBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_entry)
 
-//        coolBtn = findViewById(R.id.coolButton)
-//        mapBtn = findViewById(R.id.mapButton)
+        navbarBtn = findViewById(R.id.navbarActButton)
+        mapBtn = findViewById(R.id.mapActButton)
+        coolBtn = findViewById(R.id.rwdActButton)
     }
 }
