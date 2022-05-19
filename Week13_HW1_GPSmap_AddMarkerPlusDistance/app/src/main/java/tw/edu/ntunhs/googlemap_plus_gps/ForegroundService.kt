a5c0423dccccd61e99039584ac6e40ae27fbc17d
郵差 <twHenryYang@gmail.com>
2022-05-19 09:33:39 +0800

    Create: Week13 HW using Week12 HW

---
+++ Week13_HW1_GPSmap_AddMarkerPlusDistance/app/src/main/java/tw/edu/ntunhs/googlemap_plus_gps/ForegroundService.kt
@@ -0,0 +1,231 @@
+package tw.edu.ntunhs.googlemap_plus_gps
+
+import android.annotation.SuppressLint
+import android.app.ActivityManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.location.Location
+import android.location.LocationManager
+import android.os.*
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.location.*
+
+class ForegroundService : Service() {
+
+    // 通知頻道的名稱
+    private val CHANNEL_ID = "channel_01"
+
+    private val mBinder: IBinder = LocalBinder()
+
+    // 前台服務顯示的通知ID
+    private val NOTIFICATION_ID = 12345678
+
+    //  用於檢查綁定的活動是否真的消失了，並且在定向更改中沒有取消綁定。 僅當前者發生時，我們才創建前者服務通知。   Used to check whether the bound activity has really gone away and not unbound as part of an orientation change. We create a foreground service notification only if the former takes place.
+    private var mChangingConfiguration = false
+
+    private var mNotificationManager: NotificationManager? = null
+
+    //  FusedLocationProviderApi所需的參數
+    private var mLocationRequest: LocationRequest? = null
+    //private lateinit var mLocationRequest: LocationRequest
+
+    //  提供權限給 融合位置提供者API (Fused Location Provider API)
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+
+    // 回傳位置改變的訊息
+    private var mLocationCallback: LocationCallback? = null
+
+    private var mServiceHandler: Handler? = null
+
+    //  現在位置
+    private var mLocation: Location? = null
+
+    companion object {
+
+        private val PACKAGE_NAME = "tw.edu.ntunhs.fusedlocation"
+        private val TAG = ForegroundService::class.java.simpleName
+
+        internal val ACTION_BROADCAST = "$PACKAGE_NAME.broadcast"
+        internal val EXTRA_LOCATION = "$PACKAGE_NAME.extra.LOCATION"
+        private val EXTRA_STARTED_FROM_NOTIFICATION = "$PACKAGE_NAME.started_from_notification"
+
+    }
+
+    override fun onCreate() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mLocationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                onNewLocation(locationResult.lastLocation)
+            }
+        }
+        createLocationRequest()
+        getLastLocation()
+
+        val handlerThread = HandlerThread(TAG)
+        handlerThread.start()
+        mServiceHandler = Handler(handlerThread.looper)
+        mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager?
+
+        // Android 8+ (O)需要一個通知頻道
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = getString(R.string.app_name)
+            // 建立通知頻道
+            val mChannel =
+                NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT)
+            // 為通知管理器設定通知頻道
+            mNotificationManager!!.createNotificationChannel(mChannel)
+        }
+    }
+
+    // Service啟動時會執行    onCreate() --> onStartCommand() --> onDestroy()
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        //Log.i(TAG, "Service started");
+        val startedFromNotification = intent.getBooleanExtra(EXTRA_STARTED_FROM_NOTIFICATION, false)
+        // 若使用者(在通知中)決定移除位置更新，則刪除位置更新。      (應該無作用了，因為已將通知中的選項刪除)
+        if (startedFromNotification) {
+            removeLocationUpdates()
+            stopSelf()
+        }
+        // 告訴系統在終止服務後不要嘗試重新創建該服務。
+        return START_NOT_STICKY
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        mChangingConfiguration = true
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        // 當App(MainActivity)成為前景時，綁定服務。
+        // 發生這種情況時，服務應不再是前台服務。 (這句話的意思應該是，當App運作時，服務變成在後面執行) The service should cease to be a foreground service when that happens.
+        //Log.i(TAG, "in onBind()");
+        stopForeground(true)
+        mChangingConfiguration = false
+        return mBinder // 回傳在類別中建立的LocalBinder類別的物件
+    }
+
+    override fun onRebind(intent: Intent?) {
+        // 當App(MainActivity)再次由背景變成前景時
+        //Log.i(TAG, "in onRebind()");
+        stopForeground(true)
+        mChangingConfiguration = false
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        //Log.i(TAG, "Last client unbound from service");
+        // 當App最後與該服務解除綁定時。
+        // 如果由於MainActivity中的配置更改而調用了此方法，則我們什麼也不做。 否則，我們將此服務設為前台服務。  (這句話的意思應該是，當App不在前景運作時，服務變成在前景執行通知)    If this method is called due to a configuration change in MainActivity, we do nothing. Otherwise, we make this service a foreground service.
+//        if (!mChangingConfiguration && Utils.requestingLocationUpdates(this)) {
+//            Log.i(TAG, "Starting foreground service");
+//            startForeground(NOTIFICATION_ID, getNotification());
+//        }
+        return true
+    }
+
+    override fun onDestroy() {
+        mServiceHandler!!.removeCallbacksAndMessages(null)
+    }
+
+    //	發出位置更新的請求。
+    //@SuppressLint("MissingPermission")
+    fun requestLocationUpdates() {
+//        Log.i(TAG, "Requesting location updates");
+//        Utils.setRequestingLocationUpdates(this, true);
+        startService(Intent(getApplicationContext(), ForegroundService::class.java))
+
+        try {
+            mFusedLocationClient!!.requestLocationUpdates(
+                mLocationRequest,
+                mLocationCallback, Looper.myLooper()
+            )
+        } catch (unlikely: SecurityException) {
+//            Utils.setRequestingLocationUpdates(this, false);
+//            Log.e(TAG, "Lost location permission. Could not request updates. " + unlikely);
+        }
+    }
+
+    //	移除位置更新的請求。
+    fun removeLocationUpdates() {
+//        Log.i(TAG, "Removing location updates");
+        try {
+            mFusedLocationClient!!.removeLocationUpdates(mLocationCallback)
+            //            Utils.setRequestingLocationUpdates(this, false);
+            stopSelf()
+        } catch (unlikely: SecurityException) {
+//            Utils.setRequestingLocationUpdates(this, true);
+//            Log.e(TAG, "Lost location permission. Could not remove updates. " + unlikely);
+        }
+    }
+
+    private fun getLastLocation() {
+        try {
+            mFusedLocationClient!!.lastLocation
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful && task.result != null) {
+                        mLocation = task.result
+                    } else {
+                        //Log.w(TAG, "Failed to get location.");
+                    }
+                }
+        } catch (unlikely: SecurityException) {
+            //Log.e(TAG, "Lost location permission." + unlikely);
+        }
+    }
+
+    private fun onNewLocation(location: Location?) {
+        //Log.i(TAG, "New location: " + location);
+        mLocation = location
+
+        // 通知正在收聽有關新位置廣播的任何人。
+        val intent = Intent(ACTION_BROADCAST)
+        intent.putExtra(EXTRA_LOCATION, location)
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent)
+
+        // 如果作為前台服務運行，則更新通知內容。
+//        if (serviceIsRunningInForeground(this)) {
+//            mNotificationManager.notify(NOTIFICATION_ID, getNotification());
+//        }
+    }
+
+    //  設置位置請求參數
+    @SuppressLint("MissingPermission")
+    private fun createLocationRequest() {
+        mLocationRequest = LocationRequest()
+        mLocationRequest!!.interval = 10000 //  所需的位置更新間隔。 不精確 更新可能會或多或少地頻繁。
+        mLocationRequest!!.fastestInterval = 1000 //  活動位置更新的最快速度。 更新永遠不會比此值更頻繁。
+        // 定位方式	(0:LocationManager的GPS定位[最準確], 1:LcationRequest的HIGH_ACCURACY, 2:LcationRequest的BALANCED_POWER_ACCURACY, 3:LcationRequest的LOW_POWER, 4:LcationRequest的NO_POWER)
+        mLocationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        //mLocationRequest!!.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        //mLocationRequest!!.priority = LocationRequest.PRIORITY_LOW_POWER
+        //mLocationRequest!!.priority = LocationRequest.PRIORITY_NO_POWER
+    }
+
+    //  類別用於客戶端Binder。 由於此服務與其客戶端在同一進程中運行，因此我們不需要處理IPC。
+    inner class LocalBinder : Binder() {
+        //val service: ForegroundService
+        internal val service: ForegroundService
+            get() = this@ForegroundService
+    }
+
+    //  如果這是前景服務，則返回true。
+    fun serviceIsRunningInForeground(context: Context): Boolean {
+        val manager = context.getSystemService(
+            ACTIVITY_SERVICE
+        ) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if (javaClass.name == service.service.className) {
+                if (service.foreground) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+}
---
+++ Week13_HW1_GPSmap_AddMarkerPlusDistance/app/src/main/java/tw/edu/ntunhs/googlemap_plus_gps/MainActivity.kt
@@ -0,0 +1,291 @@
+package tw.edu.ntunhs.googlemap_plus_gps
+
+import android.Manifest
+import android.content.*
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.net.Uri
+import android.os.Bundle
+import android.os.IBinder
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+import java.util.*
+
+private lateinit var textView1: TextView
+private lateinit var longtitudeOutput: TextView
+private lateinit var latitudeOutput: TextView
+private lateinit var altitudeOutput: TextView
+private lateinit var addressOutput: TextView
+private lateinit var mMap: GoogleMap
+private lateinit var gc: Geocoder
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    // Android 10+ 定位需要改放在前景服務中
+    /// 廣播接收器 用於偵聽來自服務的廣播。
+    private lateinit var myReceiver: MyReceiver
+
+    /// 對用於獲取位置更新的服務的引用。
+    var mService: ForegroundService? = null
+
+    /// 跟踪服務的綁定狀態。
+    var ServiceFlag = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 從資源類別R中取得介面元件
+        textView1 = findViewById<TextView>(R.id.textView1)
+        textView1.text = getString(R.string.no_location_text)
+        longtitudeOutput = findViewById(R.id.longtitudeOutput)
+        latitudeOutput = findViewById(R.id.latitudeOutput)
+        altitudeOutput = findViewById(R.id.heightOutput)
+        addressOutput = findViewById(R.id.addressOutput)
+
+        // 檢查是否已取得GPS定位權限
+        if (!foregroundPermissionApproved()) {
+            requestForegroundPermissions()
+        }
+
+        myReceiver = MyReceiver()
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap //將googleMap傳給外部變數mMap，讓fun外可以使用地圖
+
+        // Add a marker in Taipei 101 and move the camera
+        val taipei101 = LatLng(25.0338, 121.5646)
+        mMap.addMarker(MarkerOptions().position(taipei101).title("Taipei 101(台北101)"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(taipei101))
+
+        // 地址顯示繁體中文
+        gc = Geocoder(this, Locale.TRADITIONAL_CHINESE)
+    }
+
+    fun moveMapToNTUNHS() {
+        // Add a marker in NTUNHS and move the camera
+        val ntunhs = LatLng(25.11787771539104, 121.52147304364689)
+        // Taiwan: 23.9037° N, 121.0794° E
+        mMap.addMarker(MarkerOptions().position(ntunhs).title("國北護校本部"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ntunhs))
+    }
+    /*
+    Google Map 顯示位置範例程式碼
+    範例1:
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(LatLng(25.118, 121.521)))
+    範例2:
+        val cameraPosition = CameraPosition.Builder()
+            .target(LatLng(25.118, 121.521)) // Sets the LatLng
+            .zoom(14f) // Sets the zoom
+            .bearing(0f) // Sets the orientation of the camera to east
+            .tilt(0f) // Sets the tilt of the camera to 30 degrees
+            .build() // Creates a CameraPosition from the builder
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+    // https://developers.google.com/maps/documentation/android-sdk/intro
+     */
+
+    override fun onStart() {
+        super.onStart()
+
+        // 綁定服務。 如果服務處於前景模式，則會向服務發出信號，因為此App處於前景，所以服務為背景。
+        val serviceIntent = Intent(this, ForegroundService::class.java)
+        bindService(serviceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onStop() {
+        if (ServiceFlag) {
+            // 取消綁定服務。 這向服務發出信號，通知此App不再為前景，服務可以通過將其升級為前景服務來進行響應。
+            unbindService(mServiceConnection)
+            ServiceFlag = false
+        }
+        super.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(myReceiver, IntentFilter(ForegroundService.ACTION_BROADCAST))
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(myReceiver)
+    }
+
+
+    // Begin ForeGround Service   Android 10+ 定位需要改放在前景服務中	/////////////
+    // 監視服務的連接狀態。
+    private val mServiceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binder = service as ForegroundService.LocalBinder
+            mService = binder.service
+            mService!!.requestLocationUpdates()
+            ServiceFlag = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            mService = null
+            ServiceFlag = false
+        }
+    }
+
+    // 主程式由此接收來自ForeGround Service 的Location資料
+    public class MyReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val location = intent.getParcelableExtra<Location>(ForegroundService.EXTRA_LOCATION)
+            if (location != null) {
+                //Toast.makeText(context, "("+location.longitude+", "+location.latitude+")", Toast.LENGTH_SHORT).show();
+                textView1.text =
+                    "(" + location.longitude + ", " + location.latitude + ", " + location.altitude + ", " + location.accuracy + ", " + convertLongToTime(
+                        location.time
+                    ) + ")"
+                // https://developer.android.com/reference/kotlin/android/location/Location#gettime
+
+                textView1.visibility = View.INVISIBLE
+                // https://stackoverflow.com/questions/49402001/how-to-set-visibility-in-kotlin
+
+                longtitudeOutput.text = "經度:" + location.longitude
+                latitudeOutput.text = "緯度:" + location.latitude
+                altitudeOutput.text = "高度:" + location.altitude
+
+                moveMapToCurrentLocation(location.latitude, location.longitude)
+
+                // 自經緯度取得地址
+                val lstAddress: List<Address> = gc.getFromLocation(location.latitude, location.longitude, 1)
+                // 擷取最主要的地理格式[0]
+                addressOutput.text = "地址:" + lstAddress[0].getAddressLine(0) +"\n"
+
+            }
+        }
+
+        private fun convertLongToTime(time: Long): Any? {
+            val date = Date(time)
+            val format = SimpleDateFormat("yyyy.MM.dd HH:mm:ss")
+            return format.format(date)
+        }
+        // https://stackoverflow.com/questions/49551461/how-can-i-convert-a-long-value-to-date-time-and-convert-current-time-to-long-kot
+
+        fun moveMapToCurrentLocation(in_latitude: Double, in_longtitude: Double) {
+            // Add a marker in current location
+            val currentLocation = LatLng(in_latitude, in_longtitude)
+            mMap.addMarker(MarkerOptions().position(currentLocation).title("您目前所在位置"))
+
+            // 使用另一種寫法，Map的鏡頭縮放等參數均可調整
+            val cameraPosition = CameraPosition.Builder()
+                .target(currentLocation) // Sets the LatLng
+                .zoom(18f) // Sets the zoom
+                .bearing(0f) // Sets the orientation of the camera to east
+                .tilt(0f) // Sets the tilt of the camera to 30 degrees
+                .build() // Creates a CameraPosition from the builder
+            mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        }
+    }
+// End ForeGround Service   Android 10+ 定位需要改放在前景服務中	/////////////
+
+    // Begin Permission	//////////////////////////////////////////////////////////////////////
+    private val TAG = "MainActivity"
+    private val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+
+    // 檢查是否已取得GPS定位權限
+    private fun foregroundPermissionApproved(): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+    // 請求GPS定位權限
+    private fun requestForegroundPermissions() {
+        val provideRationale = foregroundPermissionApproved()
+
+        if (provideRationale) {
+            Snackbar.make(
+                findViewById(R.id.activity_main),
+                R.string.permission_rationale,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.ok) {
+                    // Request permission
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+                    )
+                }.show()
+        } else {
+            Log.d(TAG, "Request foreground only permission")
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
+    // 請求定位權限的結果(回覆)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "onRequestPermissionResult")
+
+        when (requestCode) {
+            REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE -> when {
+                grantResults.isEmpty() ->
+                    // 如果請求權限時，用戶中斷對話方塊，則權限請求將被取消，並收到空陣列。
+                    Log.d(TAG, "請求權限中斷。")
+
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
+                    // 權限被核准
+                    mService?.requestLocationUpdates()
+
+                else -> {
+                    // 權限被拒絕
+                    Snackbar.make(
+                        findViewById(R.id.activity_main),
+                        R.string.permission_denied_explanation,
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setAction(R.string.settings) {
+                            // 在下方彈跳顯示一設定的對話列
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts("package", getPackageName(), null)
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }.show()
+                }
+            }
+        }
+    }
+// End Permission	////////////////////////////////////////////////////////////////////////
+
+}
