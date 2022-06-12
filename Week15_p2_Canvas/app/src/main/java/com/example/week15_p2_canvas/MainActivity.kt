2167d05b8920ce24748273c69e4f263afeb7dce3
郵差 <twHenryYang@gmail.com>
2022-06-12 09:27:49 +0800

    feat: show Canvas in App Main Page

--- Week15_p2_Canvas/app/src/main/java/com/example/week15_p2_canvas/MainActivity.kt
+++ Week15_p2_Canvas/app/src/main/java/com/example/week15_p2_canvas/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+
+        val shapeView = ShapeView(this)
+        setContentView(shapeView)
+        // 設定老師自訂的ShapeView類別(Canvas)為APP首頁的ContentView
     }
 }
