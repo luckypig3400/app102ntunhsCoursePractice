3490186dec2f40a4ba861d02d09290101d25ab3e
郵差 <twHenryYang@gmail.com>
2022-06-12 09:23:18 +0800

    import: fetch teacher's Canvas Class example code

---
+++ Week15_p2_Canvas/app/src/main/java/com/example/week15_p2_canvas/ShapeView.kt
@@ -0,0 +1,27 @@
+package com.example.week15_p2_canvas
+
+class ShapeView(context: Context?) : View(context) {
+    private var mShapeDraw: ShapeDrawable = ShapeDrawable(OvalShape())
+    private lateinit var mPaint: Paint
+    // 為以上兩變數，設定初始設定
+    init {
+        mShapeDraw.paint.color = Color.rgb(255,255,255)
+        mPaint = Paint()
+        mPaint.setAntiAlias(true)
+        mPaint.setColor(Color.CYAN)
+        mPaint.textSize = 60f
+    }
+    protected override fun onDraw(canvas: Canvas) {
+// TODO Auto-generated method stub
+        super.onDraw(canvas)
+        mShapeDraw.setBounds(10, 10, getWidth() / 2 - 10, getHeight() / 2 - 20)
+        mShapeDraw.draw(canvas)
+        canvas.drawOval(RectF(getWidth() / 2 + 10f,10f,getWidth() - 10f,getHeight() / 2 - 20f), mPaint)
+        canvas.drawText("My draw text",10f,getHeight() / 2f, mPaint)
+        canvas.drawLine(getWidth() / 2 + 10f,getHeight() / 2 - 10f,getWidth() - 10f,getHeight() / 2f, mPaint)
+        var drawImg = ResourcesCompat.getDrawable(getResources(),
+            android.R.drawable.myicon, null)!!
+        drawImg.setBounds(10,getHeight() / 2 + 10,getWidth() / 2 - 10,getHeight() * 3 / 4)
+        drawImg.draw(canvas)
+    }
+}
